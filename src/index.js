c388fbc5c96ac93420fea6bc6fdf3a53963aa776
mr-dargi <dargimohammadreza@gmail.com>
2023-09-03 17:33:32 +0330

    Add contact.js component after we click on link it show it for our contacts as new page, we add as child to root component so it show it with root and put <Outlet /> inside root to show it where we want to show it, use loader data method to receive data inside our route and show it inside our navigation sidebar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from './routes/root';
+import Root, { loader as rootLoader } from './routes/root';
 import ErrorPage from './error-page';
+import Contact from "./routes/contact";
 import './index.css';
 
 const router = createBrowserRouter([
@@ -10,8 +11,15 @@ const router = createBrowserRouter([
   path: "/",
   element: <Root />,
   errorElement: <ErrorPage/>,
+  loader: rootLoader,
+  children: [
+    {
+      path: "contacts/:contactsId",
+      element: <Contact />
+    },
+  ],
   },
-])
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
